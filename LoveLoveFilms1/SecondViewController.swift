1e90c95a1abf9d0d5b83a7bd0f679b79880798c8
Chloe Hutchings <i7243787@bournemouth.ac.uk>
2016-05-09 14:55:33 +0100

    Initial Commit

---
+++ LoveLoveFilms1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LoveLoveFilms1
+//
+//  Created by Chloe Hutchings on 09/05/2016.
+//  Copyright © 2016 Chloe Hutchings. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
